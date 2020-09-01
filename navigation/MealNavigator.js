4c71869c9587e8838ddbfc7bcb83b7435835f478
Amine Dammak <amine.dammak@habemus.com>
2020-09-01 14:48:03 +0100

    Header navigation

--- navigation/MealNavigator.js
+++ navigation/MealNavigator.js
@@ -1,13 +1,35 @@
+import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
-
-const MealNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMealsScreen: CategoryMealsScreen,
-  MealDetailScreen: MealDetailScreen,
-});
+import Colors from "../constants/Colors";
+const MealNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen,
+      /* this is here for example */
+      navigationOptions: {
+        headerStyle: {
+          backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+        },
+        headerTintColor:
+          Platform.OS === "android" ? "white" : Colors.primaryColor,
+      },
+    },
+    CategoryMeals: CategoryMealsScreen,
+    MealDetail: MealDetailScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+      },
+      headerTintColor:
+        Platform.OS === "android" ? "white" : Colors.primaryColor,
+    },
+  }
+);
 
 export default createAppContainer(MealNavigator);
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function MealDetailScreen() {
+export default function MealDetailScreen(props) {
   return (
     <View style={styles.screen}>
       <Text>MealDetailScreen screen</Text>
+      <Button
+        title="Go back to top"
+        onPress={() => props.navigation.popToTop()}
+      />
     </View>
   );
 }
